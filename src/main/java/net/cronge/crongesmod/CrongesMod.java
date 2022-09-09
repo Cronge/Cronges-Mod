ec6fd9ee14dd95a607d2264c1c831301674c521a
cronge7 <alejandroortula@gmail.com>
2022-09-09 16:55:24 +0800

    first commit

--- src/main/java/net/cronge/crongesmod/CrongesMod.java
+++ src/main/java/net/cronge/crongesmod/CrongesMod.java
@@ -3,13 +3,13 @@ package net.cronge.crongesmod;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+// Very Important Comment
 public class CrongesMod implements ModInitializer {
     public static final String MOD_ID = "crongesmod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
-
+        CrongesMod.LOGGER.info("Hello Fabric World!");
     }
 }
