60332dfe29bca5e36d14cc53adc8503136247d65
cronge7 <alejandroortula@gmail.com>
2022-09-27 09:45:58 +0800

    cleanup and improve code, tysm Daomephsta, Zero and other people who helped me!
    - Most of the stuff—e.g. strongBuffs, strongDebuffs, some RNG fields and other things you see here might be unfamiliar to you if you check(I forgot to commit the changes when I made them).
    
    - Make eightBallRNG use nextBetween() instead(so I won't have to do math). Suggestion by Daomephsta.
    
    - Remove RNG getters. Made a new getter, getRandomIndex(), that returns a random index in a list. Suggestion by Daomephsta.
    
    - Remove setters for List buffs, debuffs, strongBuffs and strongDebuffs, set them at with List.of() instead. Suggestion by Daomephsta.
    
    - Replace most RNG fields with passed parameter data in methods that need a RNG. Suggestion by Daomephsta.
    
    - Make the remaining RNG fields local in use().
    
    - Remove setCooldown and used user.getItemCooldownManager.set() instead.
    
    - Hats off to Daomephsta and ZeroNoRyouki for helping me GREATLY in improving my code.

--- src/main/java/net/cronge/crongesmod/item/custom/EightBallItem.java
+++ src/main/java/net/cronge/crongesmod/item/custom/EightBallItem.java
@@ -19,28 +19,50 @@ import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class EightBallItem extends Item {
     public EightBallItem(Settings settings) {
         super(settings);
-        this.instanceofRandom = Random.createLocal();
-        this.instanceofJavaRandom = new java.util.Random();
+        this.instanceofRandom = Random.create();
     }
 
     private final Random instanceofRandom;
-    private final java.util.Random instanceofJavaRandom;
 
-    private final List<StatusEffect> buffs = new ArrayList<>();
-    private final List<StatusEffect> debuffs = new ArrayList<>();
+    private final List<StatusEffect> buffs = List.of(
+            StatusEffects.HASTE,
+            StatusEffects.JUMP_BOOST,
+            StatusEffects.FIRE_RESISTANCE,
+            StatusEffects.WATER_BREATHING,
+            StatusEffects.INVISIBILITY,
+            StatusEffects.SATURATION,
+            StatusEffects.NIGHT_VISION,
+            StatusEffects.HEALTH_BOOST
+    );
 
-    private int smallAmplifierRNG;
-    private int bigAmplifierRNG;
+    private final List<StatusEffect> debuffs = List.of(
+            StatusEffects.SLOWNESS,
+            StatusEffects.MINING_FATIGUE,
+            StatusEffects.NAUSEA,
+            StatusEffects.HUNGER,
+            StatusEffects.POISON
+    );
 
-    private int eightBallRNG;
-    private int buffRNG;
-    private int debuffRNG;
+    private final List<StatusEffect> strongBuffs = List.of(
+            StatusEffects.STRENGTH,
+            StatusEffects.SPEED,
+            StatusEffects.RESISTANCE,
+            StatusEffects.REGENERATION,
+            StatusEffects.ABSORPTION
+    );
+    private final List<StatusEffect> strongDebuffs = List.of(
+            StatusEffects.DARKNESS,
+            StatusEffects.WITHER,
+            StatusEffects.WEAKNESS,
+            StatusEffects.BLINDNESS
+    );
+
+    // TODO: update lang file
 
     private static final String MESSAGE_ROLLED_NUMBER = "message.crongesmod.eight_ball.onUse";
     private static final String MESSAGE_ROLLED_WHEN_8 = "message.crongesmod.eight_ball.onUse_when_8";
@@ -55,117 +77,70 @@ public class EightBallItem extends Item {
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         if (world instanceof ServerWorld serverWorld && hand == Hand.MAIN_HAND) {
             user.clearStatusEffects();
-            setBuffs();
-            setDebuffs();
 
             double x = user.getX();
             double y = user.getY();
             double z = user.getZ();
 
-            eightBallRNG = instanceofRandom.nextInt((8 - 1) + 1) + 1;
-            smallAmplifierRNG = instanceofJavaRandom.nextInt(2);
-            bigAmplifierRNG = instanceofJavaRandom.nextInt((3 - 2) + 1) + 2;
-            buffRNG = instanceofJavaRandom.nextInt(buffs.size());
-            debuffRNG = instanceofJavaRandom.nextInt(debuffs.size());
+            int eightBallRNG = instanceofRandom.nextBetween(1, 8);
 
-            outputMessage(user, serverWorld, x, y, z);
-            setCooldown(this, user);
+            int smallAmplifierRNG = instanceofRandom.nextBetween(0, 1);
+            int bigAmplifierRNG = instanceofRandom.nextBetween(2, 3);
 
-            buff(user);
-            debuff(user);
+            outputMessage(user, serverWorld, x, y, z, eightBallRNG);
+            user.getItemCooldownManager().set(this, 20);
+
+            debuff(user, smallAmplifierRNG, eightBallRNG);
+            buff(user, smallAmplifierRNG, bigAmplifierRNG, eightBallRNG);
         }
         return super.use(world, user, hand);
     }
 
-    private void outputMessage(PlayerEntity player, ServerWorld serverWorld, double x, double y, double z) {
-        if (is8Rolled()) {
-            player.sendMessage(Text.translatable(MESSAGE_ROLLED_WHEN_8, getRandomNumber()));
+    private void outputMessage(PlayerEntity player, ServerWorld serverWorld, double x, double y, double z, int rolled) {
+        if (is8Rolled(rolled)) {
+            player.sendMessage(Text.translatable(MESSAGE_ROLLED_WHEN_8, rolled));
 
             spawnParticles(player, serverWorld, x, y, z);
 
-            serverWorld.playSound(null, x, y, z, SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, SoundCategory.NEUTRAL, 1.5f, -1f);
-            serverWorld.playSound(null, x, y, z, SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, SoundCategory.NEUTRAL, 2f, 1f);
+            serverWorld.playSound(null, x, y, z, SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, SoundCategory.AMBIENT, 1.5f, -1f);
+            serverWorld.playSound(null, x, y, z, SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, SoundCategory.AMBIENT, 2f, 1f);
         } else {
-            player.sendMessage(Text.translatable(MESSAGE_ROLLED_NUMBER, getRandomNumber()));
+            player.sendMessage(Text.translatable(MESSAGE_ROLLED_NUMBER, rolled));
 
-            serverWorld.playSound(null, x, y, z, SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, SoundCategory.NEUTRAL, 1.5f, 0.5f);
+            serverWorld.playSound(null, x, y, z, SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, SoundCategory.AMBIENT, 1.5f, 0.5f);
         }
     }
 
     private void spawnParticles(PlayerEntity player, ServerWorld serverWorld, double x, double y, double z) {
+        // TODO: make a colored particle
         serverWorld.spawnParticles(((ServerPlayerEntity) player), ParticleTypes.INSTANT_EFFECT, false, x + 0.3, y + 1, z, 5, 0, 1, 0, 0);
         serverWorld.spawnParticles(((ServerPlayerEntity) player), ParticleTypes.INSTANT_EFFECT, false, x - 0.3, y + 1, z, 5, 0, 1, 0, 0);
         serverWorld.spawnParticles(((ServerPlayerEntity) player), ParticleTypes.INSTANT_EFFECT, false, x, y + 1, z + 0.3, 5, 0, 1, 0, 0);
-        serverWorld.spawnParticles(((ServerPlayerEntity) player), ParticleTypes.INSTANT_EFFECT, false, x, y + 1, z  - 0.3, 5, 0, 1, 0, 0);
+        serverWorld.spawnParticles(((ServerPlayerEntity) player), ParticleTypes.INSTANT_EFFECT, false, x, y + 1, z - 0.3, 5, 0, 1, 0, 0);
     }
 
-    private boolean is8Rolled() {
-        return eightBallRNG == 8;
+    private boolean is8Rolled(int rolled) {
+        return rolled == 8;
     }
 
-    private void buff(PlayerEntity player) {
-        if (is8Rolled()) {
-            player.addStatusEffect(new StatusEffectInstance(buffs.get(getRandomBuff()), 80, getRandomBigAmplifier(), true, false, true));
+    private void buff(PlayerEntity player, int smallAmplifier, int bigAmplifier, int rolled) {
+        if (is8Rolled(rolled)) {
+            player.addStatusEffect(new StatusEffectInstance(getRandomIndex(strongBuffs), 120, bigAmplifier, true, false, true));
         } else {
-            player.addStatusEffect(new StatusEffectInstance(buffs.get(getRandomBuff()), 160, getRandomSmallAmplifier(), true, false, true));
-        }
-    }
-    private void debuff(PlayerEntity player) {
-        if (is8Rolled()) {
-            player.addStatusEffect(new StatusEffectInstance(debuffs.get(getRandomDebuff()), 80, getRandomSmallAmplifier(), true, false, true));
-        } else {
-            player.addStatusEffect(new StatusEffectInstance(debuffs.get(getRandomDebuff()), 160, getRandomSmallAmplifier() - 1, true, false, true));
+            player.addStatusEffect(new StatusEffectInstance(getRandomIndex(buffs), 160, smallAmplifier, true, false, true));
+            player.addStatusEffect(new StatusEffectInstance(getRandomIndex(buffs), 160, smallAmplifier, true, false, true));
         }
     }
 
-    private void setBuffs() {
-        buffs.add(StatusEffects.SPEED);
-        buffs.add(StatusEffects.HASTE);
-        buffs.add(StatusEffects.STRENGTH);
-        buffs.add(StatusEffects.JUMP_BOOST);
-        buffs.add(StatusEffects.REGENERATION);
-        buffs.add(StatusEffects.FIRE_RESISTANCE);
-        buffs.add(StatusEffects.WATER_BREATHING);
-        buffs.add(StatusEffects.INVISIBILITY);
-        buffs.add(StatusEffects.NIGHT_VISION);
-        buffs.add(StatusEffects.HEALTH_BOOST);
-        buffs.add(StatusEffects.ABSORPTION);
-    }
-    private void setDebuffs() {
-        debuffs.add(StatusEffects.SLOWNESS);
-        debuffs.add(StatusEffects.MINING_FATIGUE);
-        debuffs.add(StatusEffects.INSTANT_DAMAGE);
-        debuffs.add(StatusEffects.NAUSEA);
-        debuffs.add(StatusEffects.BLINDNESS);
-        debuffs.add(StatusEffects.HUNGER);
-        debuffs.add(StatusEffects.WEAKNESS);
-        debuffs.add(StatusEffects.POISON);
-        debuffs.add(StatusEffects.WITHER);
-    }
-
-    private void setCooldown(Item item, PlayerEntity player) {
-        if (is8Rolled()) {
-            player.getItemCooldownManager().set(item, 85);
+    private void debuff(PlayerEntity player, int smallAmplifier, int rolled) {
+        if (is8Rolled(rolled)) {
+            player.addStatusEffect(new StatusEffectInstance(getRandomIndex(strongDebuffs), 40, smallAmplifier, true, false, true));
         } else {
-            player.getItemCooldownManager().set(item, 165);
+            player.addStatusEffect(new StatusEffectInstance(getRandomIndex(debuffs), 160, smallAmplifier, true, false, true));
         }
     }
 
-    private int getRandomNumber() {
-        return eightBallRNG;
-    }
-
-    private int getRandomBuff() {
-        return buffRNG;
-    }
-
-    private int getRandomDebuff() {
-        return debuffRNG;
-    }
-    private int getRandomSmallAmplifier() {
-        return smallAmplifierRNG;
-    }
-    private int getRandomBigAmplifier() {
-        return bigAmplifierRNG;
+    private StatusEffect getRandomIndex(List<StatusEffect> list) {
+        return list.get(instanceofRandom.nextInt(list.size()));
     }
 }
